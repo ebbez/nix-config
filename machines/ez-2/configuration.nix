1fa7ebfb875696efb5ec56bb9bed3517f0e90e51
Ebbez <7920708+ebbez@users.noreply.github.com>
2024-11-18 20:53:24 +0100

    use networkmanager instead wpa_applicant and add hw config

--- machines/ez-2/configuration.nix
+++ machines/ez-2/configuration.nix
@@ -11,12 +11,6 @@
       ../../modules/personal-computer.nix
     ];
 
-  # Enable wifi
-  networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.xserver.libinput.enable = true;
-
   system.stateVersion = "24.05"; 
 
 }
--- machines/ez-2/hardware-configuration.nix
+++ machines/ez-2/hardware-configuration.nix
@@ -4,5 +4,71 @@
 { config, lib, pkgs, modulesPath, ... }:
 
 {
-  #tbd
+  imports =
+    [ (modulesPath + "/installer/scan/not-detected.nix")
+    ];
+
+  boot.initrd.availableKernelModules = [ "nvme" "xhci_pci" "ahci" ];
+  boot.initrd.kernelModules = [ ];
+  boot.kernelModules = [ "kvm-amd" ];
+  boot.extraModulePackages = [ ];
+
+  networking.hostName = "ez-2"
+
+  fileSystems."/" =
+    { device = "/dev/disk/by-uuid/a88bf9ed-52f3-49b8-9999-5ede238848a0";
+      fsType = "btrfs";
+      options = [ "subvol=@" "compress=zstd" "noatime" "nodiratime" ];
+    };
+
+  boot.initrd.luks.devices."root".device = "/dev/disk/by-uuid/7b5a2e5b-4624-4331-8768-d18f4375f77a";
+
+  fileSystems."/nix" =
+    { device = "/dev/disk/by-uuid/a88bf9ed-52f3-49b8-9999-5ede238848a0";
+      fsType = "btrfs";
+      options = [ "subvol=@nix" "compress=zstd" "noatime" "nodiratime" ];
+    };
+
+  fileSystems."/var/lib" =
+    { device = "/dev/disk/by-uuid/a88bf9ed-52f3-49b8-9999-5ede238848a0";
+      fsType = "btrfs";
+      options = [ "subvol=@lib" "compress=zstd" "noatime" "nodiratime" ];
+    };
+
+  fileSystems."/var/log" =
+    { device = "/dev/disk/by-uuid/a88bf9ed-52f3-49b8-9999-5ede238848a0";
+      fsType = "btrfs";
+      options = [ "subvol=@log" "compress=zstd" "noatime" "nodiratime" ];
+    };
+
+  fileSystems."/.snapshots" =
+    { device = "/dev/disk/by-uuid/a88bf9ed-52f3-49b8-9999-5ede238848a0";
+      fsType = "btrfs";
+      options = [ "subvol=@snapshots" "compress=zstd" "noatime" "nodiratime" ];
+    };
+
+  fileSystems."/home" =
+    { device = "/dev/disk/by-uuid/a88bf9ed-52f3-49b8-9999-5ede238848a0";
+      fsType = "btrfs";
+      options = [ "subvol=@home" "compress=zstd" "noatime" "nodiratime" ];
+    };
+
+  fileSystems."/boot" =
+    { device = "/dev/disk/by-uuid/D34B-1352";
+      fsType = "vfat";
+      options = [ "fmask=0022" "dmask=0022" ];
+    };
+
+  swapDevices = [ ];
+
+  # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
+  # (the default) this is the recommended approach. When using systemd-networkd it's
+  # still possible to use this option, but it's recommended to use it in conjunction
+  # with explicit per-interface declarations with `networking.interfaces.<interface>.useDHCP`.
+  networking.useDHCP = lib.mkDefault true;
+  # networking.interfaces.enp1s0.useDHCP = lib.mkDefault true;
+  # networking.interfaces.wlp2s0.useDHCP = lib.mkDefault true;
+
+  nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
+  hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
 }
